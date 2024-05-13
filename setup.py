9f538d87f3848ac61be89dfdd9c8d39484a5282c
Giovanni Fincato de Loureiro <gloureiro@luoautomation.com>
2024-05-13 17:11:29 +0200

    Adds requirements for app-service, setup for lib and the versionutil

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='lib-version',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=requirements
+)
